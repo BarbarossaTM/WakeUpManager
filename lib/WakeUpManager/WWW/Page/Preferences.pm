BarbarossaTM/WakeUpManager

commit fd24a58a1ea074eecad5284ebec9b3f6cf1a6a99

Improved any unified cookie handling in WWW:* module.
  All cookies will now be gathered in WWW.pm and pushed into the WWW::Params
  object which is pushed into WWW::Page and therefore into any existing page.

  A cookie can be retrieved via \WWW::Params->get_cookie (cookie_name).

Signed-off-by: Maximilian Wilhelm <max@rfc2324.org>

lib/WakeUpManager/WWW/Page/Preferences.pm
@@ -73,11 +73,15 @@ sub get_content_elements() {
 
 	my $content_elements = {};
 
-	my $orientation = defined $self->{cookies}->{timetable_orientation} ? $self->{cookies}->{timetable_orientation} : 'horizontal';
-	if ($orientation eq 'horizontal') {
-		$content_elements->{timetable_orientation_radio} = "<input type=\"radio\" name=\"timetable_orientation\" value=\"horizontal\" checked> $lang_hash->{horizontal} <br> <input type=\"radio\" name=\"timetable_orientation\" value=\"vertical\"> $lang_hash->{vertical} \n";
-	} else {
+	my $orientation = $self->{params}->get_cookie (timetable_orientation);
+	if (! defined $orientation) {
+		$orientation = 'horizontal';
+	}
+
+	if ($orientation eq 'vertical') {
 		$content_elements->{timetable_orientation_radio} = "<input type=\"radio\" name=\"timetable_orientation\" value=\"horizontal\"> $lang_hash->{horizontal} <br> <input type=\"radio\" name=\"timetable_orientation\" value=\"vertical\" checked> $lang_hash->{vertical}\n";
+	} else {
+		$content_elements->{timetable_orientation_radio} = "<input type=\"radio\" name=\"timetable_orientation\" value=\"horizontal\" checked> $lang_hash->{horizontal} <br> <input type=\"radio\" name=\"timetable_orientation\" value=\"vertical\"> $lang_hash->{vertical} \n";
 	}
 
 	return $content_elements;

lib/WakeUpManager/WWW/Params.pm
@@ -63,6 +63,11 @@ sub new () { # new () :  {{{
 		confess __PACKAGE__ . "->new(): No or invalid 'env' arguments.\n";
 	}
 
+	my $cookies = $args->{cookies};
+	if (! $cookies || ref ($cookies) ne 'HASH') {
+		confess __PACAKGE__ . "->new(): No or invalid 'cookies' argument.\n";
+	}
+
 	my $obj = bless {
 		debug => $debug,
 		verbose => $verbose,
@@ -71,6 +76,7 @@ sub new () { # new () :  {{{
 		page => $http_params->{page},
 		lang => $lang,
 		env => $env,
+		cookies => $cookies,
 
 		auth => $auth,
 	}, $class;
@@ -160,6 +166,23 @@ sub get_env_var ($) { # get_env_var (env_key) : ENV_value {{{
 
 	return $self->{env}->{$env_var_name};
 } # }}}
+
+sub get_cookies ($) { # get_cookie (cookies_name) : cookie_value {{{
+	my $self = shift;
+
+	my $cookie_name = shift;
+
+	if (! $self || ref ($self) ne __PACKAGE__) {
+		confess __PACKAGE__ . "->get_env_var(): Has to be called on bless'ed object.\n";
+	}
+
+	if (! defined $cookie_name) {
+		return undef;
+	}
+
+	return $self->{cookies}->{$env_var_name};
+} # }}}
+
 
 1;
 

lib/WakeUpManager/WWW.pm
@@ -81,16 +81,14 @@ sub new () { # new () :  {{{
 		auth => $obj->_get_user_info (),
 		lang => $obj->_get_lang (),
 		env => $obj->_get_all_ENV_vars (),
+		cookies => $obj->_get_all_cookies ($obj->{cgi_h}),
 	);
-
-	# Get a list of all cookies
-	$obj->{cookies} = $obj->_get_all_cookies ($obj->{cgi_h});
 
 	if (! $args->{no_header}) {
 		print $cgi_h->header ('text/html');
 	}
 	if (! $args->{contentless_mode}) {
-		$obj->{page} = WakeUpManager::WWW::Page->new (params => $obj->{params}, cookies => $obj->{cookies});
+		$obj->{page} = WakeUpManager::WWW::Page->new (params => $obj->{params});
 	}
 
 	return $obj;
@@ -106,7 +104,7 @@ sub get_page () { # get_page () : page_contents {{{
 	return $self->{page}->get_page ();
 } # }}}
 
-sub get_params () {
+sub get_params () { # {{{
 	my $self = shift;
 
 	if (! $self || ref ($self) ne __PACKAGE__) {
@@ -114,7 +112,7 @@ sub get_params () {
 	}
 
 	return $self->{http_params};
-}
+} # }}}
 
 ################################################################################
 #			internal helper functions			       #
@@ -223,7 +221,7 @@ sub _get_lang () { # _get_lang () : <lang> {{{
 	return 'en';
 } # }}}
 
-sub _get_all_ENV_vars () {
+sub _get_all_ENV_vars () { # {{{
 	my $self = shift;
 
 	if (ref ($self) ne __PACKAGE__) {
@@ -237,9 +235,9 @@ sub _get_all_ENV_vars () {
 	}
 
 	return $env;
-}
+} # }}}
 
-sub _get_POST_data () {
+sub _get_POST_data () { # {{{
 	my $self = shift;
 
 	if (ref ($self) ne __PACKAGE__) {
@@ -247,7 +245,7 @@ sub _get_POST_data () {
 	}
 
 	return $self->{cgi_h}->param('POSTDATA');
-}
+} # }}}
 
 1;
 

lib/WakeUpManager/WWW/Page.pm
@@ -59,7 +59,6 @@ sub new () { # new () :  {{{
 		verbose => $verbose,
 
 		params => $args->{params},
-		cookies => $args->{cookies},
 
 		templates => {},
 	}, $class;
@@ -91,7 +90,7 @@ sub get_page () {
 	# Get content part and optinal headers from page logic module
 	my $page_name = $self->{params}->get_page_name ();
 	if ($self->_is_valid_page ($page_name) > 0) {
-		my $page_logic = "WakeUpManager::WWW::Page::$page_name"->new (params => $self->{params}, cookies => $self->{cookies});
+		my $page_logic = "WakeUpManager::WWW::Page::$page_name"->new (params => $self->{params});
 		if ($page_logic) {
 			# Maybe there are optional header elements
 			my $page_header_elements =  $page_logic->get_header_elements ();

commit bc064c36f7ea25775c8b6bd9ab05b0a351966761

Fixed syntax errors introduced in fd24a58a1ea074eecad5284ebec9b3f6cf1a6a99
Signed-off-by: Maximilian Wilhelm <max@rfc2324.org>

lib/WakeUpManager/WWW/Page/Preferences.pm
@@ -73,7 +73,7 @@ sub get_content_elements() {
 
 	my $content_elements = {};
 
-	my $orientation = $self->{params}->get_cookie (timetable_orientation);
+	my $orientation = $self->{params}->get_cookie ('timetable_orientation');
 	if (! defined $orientation) {
 		$orientation = 'horizontal';
 	}

lib/WakeUpManager/WWW/Params.pm
@@ -65,7 +65,7 @@ sub new () { # new () :  {{{
 
 	my $cookies = $args->{cookies};
 	if (! $cookies || ref ($cookies) ne 'HASH') {
-		confess __PACAKGE__ . "->new(): No or invalid 'cookies' argument.\n";
+		confess __PACKAGE__ . "->new(): No or invalid 'cookies' argument.\n";
 	}
 
 	my $obj = bless {
@@ -167,7 +167,7 @@ sub get_env_var ($) { # get_env_var (env_key) : ENV_value {{{
 	return $self->{env}->{$env_var_name};
 } # }}}
 
-sub get_cookies ($) { # get_cookie (cookies_name) : cookie_value {{{
+sub get_cookie ($) { # get_cookie (cookies_name) : cookie_value {{{
 	my $self = shift;
 
 	my $cookie_name = shift;
@@ -180,7 +180,7 @@ sub get_cookies ($) { # get_cookie (cookies_name) : cookie_value {{{
 		return undef;
 	}
 
-	return $self->{cookies}->{$env_var_name};
+	return $self->{cookies}->{$cookie_name};
 } # }}}
 
 