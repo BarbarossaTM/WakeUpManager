BarbarossaTM/WakeUpManager

commit 18b1c500e5e7aafcaa3fcfdba4f661e55ab1676f

HostDB: Removed redundant right name specification.
Signed-off-by: Maximilian Wilhelm <max@rfc2324.org>

lib/WakeUpManager/DB/HostDB.pm
@@ -16,7 +16,16 @@ use WakeUpManager::Common::Utils qw(:time);
 my $true  = (1 == 1);
 my $false = (1 != 1);
 
-##
+#
+# A list of names which exist in the WUM database.
+# Used to verify ACL query parameters.
+my $valid_rights = {
+	'allow_boot' => 1,
+	'read_config' => 1,
+	'write_config' => 1,
+};
+
+#
 # Little bit of magic to simplify debugging
 sub _options(@) { # _options (@) : \% {{{
 	my %ret = @_;
@@ -493,7 +502,7 @@ sub _user_has_right_on_host ($$$) { # _user_has_right_on_host (uid, host_id, rig
 	return undef if (ref ($self) ne __PACKAGE__);
 
 	# Do we know about the requested right?
-	if (! $right =~ m/^(allow_boot|read_config|write_config)$/) {
+	if (! defined $valid_right->{$right}) {
 		return undef;
 	}
 
@@ -607,7 +616,7 @@ sub _user_has_right_on_hostgroup ($$$) { # _user_has_right_on_hostgroup (uid, ri
 	return undef if (ref ($self) ne __PACKAGE__);
 
 	# Do we know about the requested right?
-	if (! $right =~ m/^(allow_boot|read_config|write_config)$/) {
+	if (! defined $valid_right->{$right}) {
 		return undef;
 	}
 
@@ -716,7 +725,7 @@ sub _hosts_user_has_right_on ($$) { # _hosts_user_has_right_on (uid, right) : \%
 	return undef if (ref ($self) ne __PACKAGE__);
 
 	# Do we know about the requested right?
-	if (! $right =~ m/^(allow_boot|read_config|write_config)$/) {
+	if (! defined $valid_right->{$right}) {
 		return undef;
 	}
 
@@ -812,7 +821,7 @@ sub _hosts_of_hostgroup_user_has_right_on ($$) { # _hosts_user_has_right_on (hos
 	}
 
 	# Do we know about the requested right?
-	if (! $right =~ m/^(allow_boot|read_config|write_config)$/) {
+	if (! defined $valid_right->{$right}) {
 		return undef;
 	}
 
@@ -880,7 +889,7 @@ sub _hostgroups_user_has_right_on ($$) { # _hostgroups_user_has_right_on (uid, r
 	return undef if (ref ($self) ne __PACKAGE__);
 
 	# Do we know about the requested right?
-	if (! $right =~ m/^(allow_boot|read_config|write_config)$/) {
+	if (! defined $valid_right->{$right}) {
 		return undef;
 	}
 

commit c7f23335b9ae543ef56de7b561e7429f4475ffc3

HostDB: Fixed typo introduced in 18b1c500e5e7aafcaa3fcfdba4f661e55ab1676f
Signed-off-by: Maximilian Wilhelm <max@rfc2324.org>

lib/WakeUpManager/DB/HostDB.pm
@@ -502,7 +502,7 @@ sub _user_has_right_on_host ($$$) { # _user_has_right_on_host (uid, host_id, rig
 	return undef if (ref ($self) ne __PACKAGE__);
 
 	# Do we know about the requested right?
-	if (! defined $valid_right->{$right}) {
+	if (! defined $valid_rights->{$right}) {
 		return undef;
 	}
 
@@ -616,7 +616,7 @@ sub _user_has_right_on_hostgroup ($$$) { # _user_has_right_on_hostgroup (uid, ri
 	return undef if (ref ($self) ne __PACKAGE__);
 
 	# Do we know about the requested right?
-	if (! defined $valid_right->{$right}) {
+	if (! defined $valid_rights->{$right}) {
 		return undef;
 	}
 
@@ -725,7 +725,7 @@ sub _hosts_user_has_right_on ($$) { # _hosts_user_has_right_on (uid, right) : \%
 	return undef if (ref ($self) ne __PACKAGE__);
 
 	# Do we know about the requested right?
-	if (! defined $valid_right->{$right}) {
+	if (! defined $valid_rights->{$right}) {
 		return undef;
 	}
 
@@ -821,7 +821,7 @@ sub _hosts_of_hostgroup_user_has_right_on ($$) { # _hosts_user_has_right_on (hos
 	}
 
 	# Do we know about the requested right?
-	if (! defined $valid_right->{$right}) {
+	if (! defined $valid_rights->{$right}) {
 		return undef;
 	}
 
@@ -889,7 +889,7 @@ sub _hostgroups_user_has_right_on ($$) { # _hostgroups_user_has_right_on (uid, r
 	return undef if (ref ($self) ne __PACKAGE__);
 
 	# Do we know about the requested right?
-	if (! defined $valid_right->{$right}) {
+	if (! defined $valid_rights->{$right}) {
 		return undef;
 	}
 