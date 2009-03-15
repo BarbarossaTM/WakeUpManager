91917aa46a4c5b782579ebdfdfff22c91ed5d090
Maximilian Wilhelm <max@rfc2324.org>
2009-03-15 15:02:18 +0100

    Make WakeUpManager GPLv2
      Added GPLv2 stanza into header of all modules and scripts.
    
    Signed-off-by: Maximilian Wilhelm <max@rfc2324.org>

--- lib/WakeUpManager/Agent/Connector.pm
+++ lib/WakeUpManager/Agent/Connector.pm
@@ -1,11 +1,27 @@
 #!/usr/bin/perl -WT
 #
-#  This class is part of the WakeUpManager suite.
+#   WakeUpManager suite
 #
+#   Copyright (C) 2008-2009 Maximilian Wilhelm <max@rfc2324.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+##
 #  This class is the interface which should be used by programmers to boot
 #  a specific maschine.
 #
-#  This call will lookup the host in the HostDB, figure out which agent to
+#  This class will lookup the host in the HostDB, figure out which agent to
 #  trigger for booting the host and actually make the ageht to send the
 #  wakeup packet.
 #
